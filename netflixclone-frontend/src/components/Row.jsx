27b62a22cc2981e69fa65a93febc4c34254622b7
umair shah <umairshah14@hotmail.com>
2023-04-06 02:25:14 +0100

    adding icons for scrolling and liking

--- netflixclone-frontend/src/components/Row.jsx
+++ netflixclone-frontend/src/components/Row.jsx
@@ -1,11 +1,10 @@
 import axios from "axios";
-// import {FaHeart, FaRegHeart} from "react-icons/fa";
 import React, { useEffect, useState } from "react";
+import Movie from "./Movie";
+import {MDChevronLeft, MDChevronRight} from 'react-icons'
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-//   const [like, setLike] = useState(false);
-
 
   useEffect(() => {
     axios.get(fetchURL).then((response) => {
@@ -19,18 +18,10 @@ const Row = ({ title, fetchURL }) => {
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
-        <div id={"slider"}>
+        <div id={"slider"} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative ">
             {movies.map((item, id) => (
-                <div className="w-[160px] sm:w-[240px] md:w-240px] lg:w-[280px] inline-block cursor-pointer relative p-2 ">
-                    <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}></img>
-                    <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
-                        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center"> {item?.title} </p>
-                    </div>
-
-                </div>
-
+                <Movie key={id} item={item} />
             ))}
-        
         </div>
       </div>
     </>
