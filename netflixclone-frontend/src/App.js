efb0c55ab44658f12b6e0f6972a200f169463ba2
umair shah <umairshah14@hotmail.com>
2023-04-06 01:15:55 +0100

    main and navbar components made

---
+++ netflixclone-frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    
+    </>
+  );
+}
+
+export default App;
