80d1e9438dc83dd16d67249057f110b3a1556417
umair shah <umairshah14@hotmail.com>
2023-04-06 02:10:12 +0100

    row component styling added succesfully

--- netflixclone-frontend/src/components/Row.jsx
+++ netflixclone-frontend/src/components/Row.jsx
@@ -1,10 +1,10 @@
 import axios from "axios";
-import {FaHeart, FaRegHeart} from "react-icons/fa";
+// import {FaHeart, FaRegHeart} from "react-icons/fa";
 import React, { useEffect, useState } from "react";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
-  const [like, setLike] = useState(false);
+//   const [like, setLike] = useState(false);
 
 
   useEffect(() => {
@@ -13,25 +13,24 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  console.log(movies);
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center">
         <div id={"slider"}>
-          {movies.map((item, id) => (
-      
-              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2" key={id}>
-                <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}/>
-                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:bg-opacity-100 text-white">
-                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
-                    {item?.title}
-                  </p>
-                  <p>
-                    {like ? <FaHeart/> : <FaRegHeart/>}
-                  </p>
+            {movies.map((item, id) => (
+                <div className="w-[160px] sm:w-[240px] md:w-240px] lg:w-[280px] inline-block cursor-pointer relative p-2 ">
+                    <img className="w-full h-auto block" src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`} alt={item?.title}></img>
+                    <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                        <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center"> {item?.title} </p>
+                    </div>
+
                 </div>
-              </div> 
-          ))}
+
+            ))}
+        
         </div>
       </div>
     </>
