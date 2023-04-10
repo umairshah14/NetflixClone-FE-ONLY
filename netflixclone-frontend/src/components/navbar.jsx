0e2a54a9857326c437063fb01e34eddc635b7e4d
umair shah <umairshah14@hotmail.com>
2023-04-10 16:25:05 +0100

    All working ready to deploy

--- netflixclone-frontend/src/components/navbar.jsx
+++ netflixclone-frontend/src/components/navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className="flex items-center justify-between p-4 z-[100] w-full absolute">
       <Link to="/">
         <h1 className="text-red-600 text-4xl font-bold cursor-pointer">
-          NETFLIX
+          CLONEFLIX
         </h1>
       </Link>
       {/* <img src='./images/logo.png'/> */}
