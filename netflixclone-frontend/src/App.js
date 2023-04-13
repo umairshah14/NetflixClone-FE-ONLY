bb431bb0feaa48d9e0d47780c148d1f4b39c5d52
umair shah <umairshah14@hotmail.com>
2023-04-13 16:23:46 +0100

    setup gh-pages

--- netflixclone-frontend/src/App.js
+++ netflixclone-frontend/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+
 import Navbar from "./components/navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import Home from "./pages/Home";
